7df512b72c10a4ddb7ce15e3481823003214e60c
Ben Giles <btgiles@gmail.com>
2021-11-27 23:08:28 +0000

    Add clang-format and more Python examples

--- examples/monty.py
+++ examples/monty.py
@@ -1,20 +1,33 @@
-# Type hints
+# Python basics
+
+## Type hints
 # type: ignore
 
-# Importing
+## Importing
 import re as regex
 import matplotlib.pyplot as plt
+from pprint import pprint
+import importlib
 
-# Muliline strings
+## Muliline strings
 s = """9noop9
-afoo999
+afOo999
 abar9001"""
 
-# Regular expressions
-my_regex = regex.compile("^[a-z](.*?)\d(3,)$", regex.I | regex.M)
+## Regular expressions
+# TODO: doesn't match last digit
+my_regex = regex.compile("^[a-z](.*?)\d$", regex.I | regex.M)
+pprint(my_regex.findall(s))
 
-# Looping and iterators
+## Looping and iterators
 for my_match in my_regex.finditer(s):
     print(my_match.group(1))
 
-    
+## Whitespace
+# TODO: black should format matrices like a gentleman
+eye = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+
+big_s = (
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+)
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <string>
+#include <stdio>
+
+void
+main(int argc, char **argv){ std::cout << "Hello, world" << std::endl; return 0; }
